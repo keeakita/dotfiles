371bf6469343877090271521207a7dc1d91d38be
LinuxBrony <techdude300@gmail.com>
2013-04-27 00:04:04 -0400

    make zsh check for cowsay and ponysay

--- oh-my-zsh-custom/_pony.zsh
+++ oh-my-zsh-custom/_pony.zsh
@@ -1,6 +1,6 @@
 # Ponies! (or cows)
-if [[ $TERM == "xterm-256color" ]]; then
+if [[ $TERM == "xterm-256color" ]] && command -v ponysay > /dev/null 2>&1; then
     fortune | ponysay -W 77
-else
+elif command -v cowsay  > /dev/null 2>&1 ; then
     fortune | cowsay -W 77
 fi
