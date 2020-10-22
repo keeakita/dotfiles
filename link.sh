5361f4a41a4de628a2827716d580b97db11909e9
William Osler <william@oslers.us>
2020-10-21 18:38:28 -0700

    link.sh - formatting & best practice

--- link.sh
+++ link.sh
@@ -1,30 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 # Backs up old dotfiles and symlinks all the new ones
 
 set -u
 
-BACKUPDIR=$HOME/dotfile_backups # Don't end this with a /
+BACKUPDIR="$HOME"/dotfile_backups # Don't end this with a /
 
-cd $HOME
+cd "$HOME"
 
 # Make the backup dir if it doesn't exist
-if [ ! -d $BACKUPDIR ]; then
-    mkdir -p $BACKUPDIR
+if [ ! -d "$BACKUPDIR" ]; then
+    mkdir -p "$BACKUPDIR"
 fi
 
-for f in $HOME/dotfiles/*; do
-    if ! echo $f | grep -q $HOME/dotfiles/README.md && ! echo $f | grep -q $HOME/dotfiles/link.sh; then
+for f in "$HOME"/dotfiles/*; do
+    if ! echo "$f" | grep -q "$HOME"/dotfiles/README.md && ! echo "$f" | grep -q "$HOME"/dotfiles/link.sh; then
         # We use \0 since it's the only invalid filename character on ext4
-        BASENAME=$(echo $f | sed -e s\0$HOME/dotfiles/\0\0;) 
-        ORIGFILE=$HOME/.$BASENAME
-        BACKUPFILE=$BACKUPDIR/.$BASENAME
+        BASENAME=$(echo "$f" | sed -e s\0"$HOME"/dotfiles/\0\0;) 
+        ORIGFILE="$HOME"/."$BASENAME"
+        BACKUPFILE="$BACKUPDIR"/."$BASENAME"
 
-        if [ ! -L $ORIGFILE ]; then
-            mv $ORIGFILE $BACKUPFILE
+        if [ ! -L "$ORIGFILE" ]; then
+            mv "$ORIGFILE" "$BACKUPFILE"
         fi
 
         # Now we symlink
-        ln -s -T $f $ORIGFILE
+        ln -s -T "$f" "$ORIGFILE"
     fi
 done
 
