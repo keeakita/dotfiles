48a82baa50137552c25a582734fc388ebdaea91f
William Osler <william@oslers.us>
2020-10-21 21:47:11 -0700

    zsh: fix agnoster git dirty prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,4 +1,7 @@
-# Customize the agnoster theme. Has to be here since themes are the last thing that gets loaded.
+ZSH_THEME_GIT_PROMPT_DIRTY='*'
+source "$ZDOTDIR"/thirdparty/git.zsh
+source "$ZDOTDIR"/thirdparty/agnoster.zsh-theme
+
 setopt prompt_subst
 export SEGMENT_SEPARATOR=''
 export PS1='%{%f%b%k%}$(build_prompt)
